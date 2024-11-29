6b36655101bf756934c73848686979f6ece8915c
Beyza Sezer <byzaszr@outlook.com>
2024-11-29 17:55:01 +0300

    adding teacher calendar page

--- GEPS/Controllers/HomeController.cs
+++ GEPS/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace GEPS.Controllers
             return View();
         }
 
+        public IActionResult TeacherCalendar()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
