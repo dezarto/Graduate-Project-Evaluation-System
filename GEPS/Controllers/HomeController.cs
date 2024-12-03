e0bac8c91fff0b48e57a6a8e29b045e888a5f0fd
Beyza Sezer <byzaszr@outlook.com>
2024-12-03 18:11:36 +0300

    adding teacher approve project part
    +letter correction of teacher vieW result instead of wieR result

--- GEPS/Controllers/HomeController.cs
+++ GEPS/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace GEPS.Controllers
         {
             return View();
         }
+        public IActionResult TeacherApproveProject()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
